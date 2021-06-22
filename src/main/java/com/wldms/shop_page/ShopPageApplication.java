efba2ca8a3bbcfd90e67c1a7fa64a0ed88e9cf57
wldmsKim <tmqpsoi@gmail.com>
2021-06-22 11:29:02 +0900

    fitst commit

---
+++ src/main/java/com/wldms/shop_page/ShopPageApplication.java
@@ -0,0 +1,13 @@
+package com.wldms.shop_page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopPageApplication.class, args);
+    }
+
+}
